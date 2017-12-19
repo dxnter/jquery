510308a7646f49490c5f89b0b7c1f5cb8b52aefd
Danny <contact@danfoster.io>
2017-12-19 15:53:08 -0600

    Ninja to Cat final

---
+++ ninja-to-cat/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('#num0').click(function () {
+        $('img', this).toggle();
+    });
+    $('#num1').click(function () {
+        $('img', this).toggle();
+    });
+    $('#num2').click(function () {
+        $('img', this).toggle();
+    });
+    $('#num3').click(function () {
+        $('img', this).toggle();
+    });
+    $('#num4').click(function () {
+        $('img', this).toggle();
+    });
+});
